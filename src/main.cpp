01825010046ad7b3d87c55f0f13256796b260038
Alan Aristizabal <soyalanarist@gmail.com>
2026-01-13 09:18:43 -0500

    initial project setup with CMake, SFML, and basic window functioning

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode(800, 600),
+        "Sumo Balls"
+    );
+
+    window.setFramerateLimit(60);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
