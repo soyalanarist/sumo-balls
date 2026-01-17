0e1de7cc109e23a0083d3d397eeb60d74f5454bb
Alan Aristizabal <soyalanarist@gmail.com>
2026-01-17 09:06:45 -0500

    refactor: remove old file locations after restructuring
    - Remove old src/game/Arena.cpp and src/game/Arena.h (moved to entities/)
    - Remove old src/game/Player.cpp and src/game/Player.h (no longer needed)
    - Remove old src/game/menus/ structure (moved to src/screens/menus/)
    - Clean up directory structure after major refactoring

--- src/game/menus/Menu.h
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-
-class Menu {
-public:
-    virtual ~Menu() = default;
-    virtual void processInput(sf::RenderWindow& window) = 0;
-    virtual void update(float dt) = 0;
-    virtual void render(sf::RenderWindow& window) = 0;
-};
